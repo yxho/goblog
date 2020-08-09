c70a44600c640376501eff492977ae2e40681290
yxh <417670161@qq.com>
2020-08-09 21:52:32 +0800

    Write user module interface to realize preliminary verification and paging function.

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,6 +1,14 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"goblog/model"
+	"goblog/utils/errmsg"
+	"net/http"
+	"strconv"
+)
+
+var code int
 
 // 查询用户是否存在
 func UserExist(c *gin.Context) {
@@ -10,13 +18,42 @@ func UserExist(c *gin.Context) {
 // 添加用户
 func AddUser(c *gin.Context) {
 	// todo 添加用户
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCES {
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		code = errmsg.ERROR_USERNAME_USED
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+
 }
 
 // 查询单个用户
 
 // 查询用户列表
 func GetUsers(c *gin.Context) {
-
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	if pageSize == 0 {
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = -1
+	}
+	data := model.GetUsers(pageSize, pageNum)
+	code = errmsg.SUCCES
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // 编辑用户
--- model/User.go
+++ model/User.go
@@ -1,12 +1,49 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"goblog/utils/errmsg"
+)
 
 type User struct {
 	gorm.Model
-	Username string  `gorm:"type:varchar(20);not null" json:"username"`
+	Username string `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
-	Role int `gorm:"type:int" json:"role"`
+	Role     int    `gorm:"type:int" json:"role"`
 }
 
-//
+// 查询用户是否存在
+func CheckUser(name string) (code int) {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+	if users.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED // 1001
+	}
+	return errmsg.SUCCES
+}
+
+// 新增用户
+func CreateUser(data *User) int {
+	// todo 添加用户
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCES
+}
+
+// 查询用户列表
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err = db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return users
+}
+
+// 编辑用户
+
+
+// 删除用户
+
